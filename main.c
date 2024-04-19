71809a7fef3541417cf3bc5e61e71fd91dcf8ee9
Cag202 <165748880+Cag202@users.noreply.github.com>
2024-04-18 22:09:39 -0400

    Update main.c
    Took out most of old lines, Added 3rd LED control, Motors are fixed, adjusted pump pwm values, fixed ADC_Conversion(2), put water level monitoring

--- main.c
+++ main.c
@@ -49,8 +49,8 @@ int main(void)
 	PORTC = PORTC | (1<<PC4 | 1<<PC5);
 	PORTC = PORTC ^ 0<<PC3; //turning off the LED's and motor to start
 	
-	DDRD = 0b01110000; // PD5 and PD6 PWM 0, D7 and D3 nothing on them so 0, D4 1 cause direction, D2 0 cause interrupt is input, switches are set as outputs
-	PORTD = PORTD & 0<<PD4;
+	DDRD = 0b1110000; // PD5 and PD6 PWM 0, D7 and D3 nothing on them so 0, D4 1 cause direction, D2 0 cause interrupt is input, switches are set as outputs
+	PORTD = PORTD & 0<<PD4 & 0<<PD7; //turninng off motor and LED
 	
 	DDRB = 0b00101111; // 7 Segment is PB2,3,5 PB0,1 are motor pins
 	PORTB = PORTB & 0<<PB0 & 0<<PB1; //setting motors pins off to start
@@ -78,8 +78,8 @@ int main(void)
 	// Variables
 	int result;
 	char calibration_flag = 1; //test flag for check in purposes
-	volatile float water_warning_lvl = 30.0 / d_per_lvl;
-	volatile float moisture_content;\
+	volatile float water_warning_lvl = 235;
+	volatile float moisture_content;
 	int t = 0; //time keeping variable 
 	
     while (1) 
@@ -91,43 +91,13 @@ int main(void)
 		send_to_MAX7221(0b00001100,0b00000001); //turn on the display DIG 0
 		//send_to_MAX7221(0b00001100,0b00000010); //turn on the display DIG 1
 
-	   	send_to_MAX7221(0b00000001,0b00000001); //binary 0 on DIG0
-		send_to_MAX7221(0b00000010,0b00000010); //binary 0 on DIG1
+	   	send_to_MAX7221(0b00000001,0b00000000); //binary 0 on DIG0
+		send_to_MAX7221(0b00000010,0b00000000); //binary 0 on DIG1
 		//wait(1000);
 		
 		//key notes: potentiometer has range of 270 degrees with 255 levels to give 1.0588 degrees/level
 		//midpoint is roughly level 128 (actual value 127.5)
 		// 45 degree rotation would be 42.5 steps, will round to 43 steps
-		
-		//test code for check in 2, remove before final push
-		/*while(flag){
-			//get initial position and start motor in forward direction
-			result = ADC_Conversion(3);
-			motor_run(1,1,125); //motor on forward at "full speed"
-			motor_run(2,1,50); //pump on at "full flow"
-			//control to see when angle changes
-			while(result < 43){ //going to start from 0 degrees and then rotate to 45 degrees by hand
-				result = ADC_Conversion(3);
-				if(result > 35){
-					motor_run(1,1,50); //reducing speed of motor as specified in above goals
-					motor_run(2,1,25); //reducing pump flow per specified goal
-				}
-				wait(25); //allows motor to continue running 
-			} // Motor hits first full movement
-			motor_off(3); //turning off motor and pump to reduce jerk
-			wait(25);
-			motor_run(1,0,100); //turning motor back on in reverse direction at "full speed"
-			motor_run(2,1,50); //turning pump back on at full flow
-			while(result > 0) {
-				result = ADC_Conversion(3);
-				if(result < 8){
-					motor_run(1,0,50); //reducing speed of motor as specified in above goals
-					motor_run(2,1,25); //reducing pump flow per specified goal
-				}
-			}
-			motor_off(3);
-			flag = 0;
-			*/
 		
 		//Start final product code
 		
@@ -154,6 +124,7 @@ int main(void)
 		}
 		water_cycle();
 		PORTC = (!(PORTC | 1<<PC5)); //Turning on full use light
+		PORTC = PORTC | 1<<PC4; //turning off red LED (in case above line turns on
 			
 		
 		//while(1) loop for steady state operation, 
@@ -175,6 +146,7 @@ int main(void)
 			//test line
 			wait(10);
 			moisture_content = 100 - moisture_content;
+			volatile float moisture_value = moisture_content;
 			wait(10);
 			//need to separate moisture_content into 2 integers.
 			while(moisture_content > 10) {
@@ -190,8 +162,17 @@ int main(void)
 			send_to_MAX7221(0b00000001,i); //display 10's place on dig 0
 			send_to_MAX7221(0b00000010,j); // display 1's place on dig 1
 			t = 60; // 60 second delay so water can permeate through soil
-			if(moisture_content < moisture_threshold) {
+			if(moisture_value < moisture_threshold) {
 				while(t > 0) {
+					//water reservoir level
+					result = ADC_Conversion(2);
+					if(result > water_warning_lvl) {
+						PORTC = ((PORTC | 1<<PC4));
+						PORTD = (!(PORTD | 1<<PD7));
+						} else {
+						PORTC = (!(PORTC | 1<<PC4));
+						PORTD = PORTD | 1<<PD7;
+					}
 					wait(1000);
 					t--;
 				}
@@ -200,10 +181,12 @@ int main(void)
 			
 			//water reservoir level
 			result = ADC_Conversion(2);
-			if(result < water_warning_lvl) {
-				PORTC = (!(PORTC | 1<<PC4));
+			if(result > water_warning_lvl) {
+				PORTC = ((PORTC | 1<<PC4));
+				PORTD = (!(PORTD | 1<<PD7));
 			} else {
-				PORTC = (!(PORTC | 0<<PC5));
+				PORTC = (!(PORTC | 1<<PC4));
+				PORTD = PORTD | 1<<PD7;
 			}
 			
 			wait(1000);
@@ -296,7 +279,7 @@ int ADC_Conversion(char pin_choice){
 	}
 	if(pin_choice == 2){
 		//set adc to happen at water level
-		ADMUX = ADMUX | 0<<MUX0 | 1<<MUX1 | 0<<MUX2 | 0<<MUX3;
+		ADMUX = ADMUX | 1<<MUX0 | 0<<MUX1 | 0<<MUX2 | 0<<MUX3;
 	}
 	if(pin_choice == 3){
 		//set adc to happen at rotation potentiometer ie mux bits 0010
@@ -346,15 +329,16 @@ void motor_off(char motor){
 void calibration(){
 	//start by getting pwm_base value.
 	motor_off(3);
-	float position = ADC_Conversion(3); //getting initial position in steps.
-	float angular_V = ((ADC_Conversion(3)-position)*d_per_lvl)/2.0; //getting angular velocity for 2 second pulse of motor
+	volatile float position = ADC_Conversion(3); //getting initial position in steps.
+	volatile float angular_V = ((ADC_Conversion(3)-position)*d_per_lvl)/2.0; //getting angular velocity for 2 second pulse of motor
 	char pwm = 100;
 	while (((angular_V) < 3.0) && (angular_V > -3)) { //3.0 degrees/s is the wanted velocity based on motor analysis document
 		position = ADC_Conversion(3);
 		motor_run(1,1,pwm);
 		wait(2000);
 		motor_off(1);
-		angular_V = ((ADC_Conversion(3)-position)*d_per_lvl)/2.0;
+		float new_position = ADC_Conversion(3);
+		angular_V = ((new_position-position)*d_per_lvl)/2.0;
 		motor_run(1,0,pwm);
 		wait(2000);
 		motor_off(1);
@@ -389,50 +373,60 @@ void calibration(){
 	} motor_off(1);
 }
 
+//for water cycle switched all motor calls from 1 to 0 and visa versa, that may fix it
 void water_cycle(){
 	//goals
 	//rotate motor 45 degrees with pump on, slow motor and pump around the corner, reverse the direction, return to origin, then do on opposite side.
-	char position = ADC_Conversion(3);
+	volatile char position = ADC_Conversion(3);
 	//moving (+) 45 deg first
 	while (position < 171){
 		if((171 - position) < 8) {
 			motor_run(1,1,(pwm_base-15));
-			motor_run(2,1,50);
+			motor_run(2,1,85);
 			wait(500);
+			motor_off(3);
 			position = ADC_Conversion(3);
 		} else {
 			motor_run(1,1,pwm_base);
-			motor_run(2,1,75);
+			motor_run(2,1,95);
 			wait(500);
+			motor_off(3);
 			position = ADC_Conversion(3);
 		}
+		position = ADC_Conversion(3);
 	} //end of first movement 
 	//moving (-) 90 deg
 	while(position > 85) {
 		if((position - 85 < 8)){
 			motor_run(1,0,(pwm_base-15));
-			motor_run(2,1,50);
+			motor_run(2,1,85);
 			wait(500);
+			motor_off(3);
 			position = ADC_Conversion(3);
 		} else {
 			motor_run(1,0,pwm_base);
-			motor_run(2,1,75);
+			motor_run(2,1,95);
 			wait(500);
+			motor_off(3);
 			position = ADC_Conversion(3);
 		}
+		position = ADC_Conversion(3);
 	} //end of second movement
 	while(position < 128) {
 		if((128 - position) < 8) {
 			motor_run(1,1,(pwm_base-15));
-			motor_run(2,1,50);
+			motor_run(2,1,85);
 			wait(500);
+			motor_off(3);
 			position = ADC_Conversion(3);
 		} else {
 			motor_run(1,1,pwm_base);
-			motor_run(2,1,75);
+			motor_run(2,1,95);
 			wait(500);
+			motor_off(3);
 			position = ADC_Conversion(3);
 		}
+		position = ADC_Conversion(3);
 	}
 	motor_off(3);
 } //End of function
